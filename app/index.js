da28311364f3048b77143478d950322f41ffef2a
Qian He <github@heqian.name>
2017-07-28 23:27:36 -0700

    Add support for general IFTTT Webhooks

--- app/index.js
+++ app/index.js
@@ -1,25 +1,60 @@
 'use strict'
 
+var key
+
 da.segment.onpreprocess = function (trigger, args) {
+    console.log('[onpreprocess]', { trigger: trigger, args: args })
     da.startSegment(trigger, args)
 }
 
 da.segment.onstart = function (trigger, args) {
-    console.log('onstart', { trigger: trigger, args: args })
+    console.log('[onstart]', { trigger: trigger, args: args })
     da.getSegmentConfig({
         onsuccess: function (config) {
-            console.log('getSegmentConfig succeeded', config)
-            ifttt(config)
+            console.log('[getSegmentConfig: onsuccess]', config)
+            if (config.key) {
+                key = config.key
+                var speechToText = new da.SpeechToText()
+                speechToText.startSpeechToText(generateCommand)
+            } else {
+                speak('Please configure your IFTTT key in the app.')
+            }
         },
+
         onerror: function (error) {
             console.log('getSegmentConfig failed')
+            speak('Failed to get the segment configuration.')
         }
     })
 }
 
-function ifttt(config) {
-    var url = da.getString('apiFormat', config)
-    var body = { value1: config.value1, value2: config.value2, value3: config.value3 }
+var generateCommand = {
+    onsuccess: function (results) {
+        console.log('[startSpeechToText: onsuccess]', results)
+        var dataIndex = results.length
+        for (var i = 1; i < results.length - 2; i++) {  // Has to have at least one word before and one word after phrase 'with data'
+            if (results[i] === 'with' && results[i + 1] === 'data') {
+                dataIndex = i
+                break
+            }
+        }
+        var event = results.slice(0, dataIndex).join('_').toLowerCase()
+        var commands = results.slice(dataIndex + 2)
+        var value1 = commands[0]
+        var value2 = commands[1]
+        var value3 = commands[2]
+        ifttt(key, event, value1, value2, value3)
+    },
+
+    onerror: function (error) {
+        console.log('[startSpeechToText: onerror]', error)
+        speak(error.messsage)
+    }
+}
+
+function ifttt(key, event, value1, value2, value3) {
+    var url = da.getString('apiFormat', { key: key, event: event })
+    var body = { value1: value1, value2: value2, value3: value3 }
 
     var xhr = da.getXhr()
     xhr.open('POST', url, true)
@@ -27,9 +62,9 @@ function ifttt(config) {
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
-                speak('done')
+                speak('Command has been sent.')
             } else {
-                speak('failed')
+                speak('Failed to send command.')
             }
         }
     }
@@ -40,15 +75,15 @@ function speak(text, stop = true) {
     var synthesis = da.SpeechSynthesis.getInstance()
     synthesis.speak(text, {
         onstart: function () {
-            console.log('speak start')
+            console.log('[speak: onstart]')
         },
         onend: function () {
-            console.log('speak onend')
+            console.log('[speak: onend]')
             if (stop) da.stopSegment()
         },
         onerror: function (error) {
-            console.log('speak cancel: ' + error.messsage)
-            if (stop) da.stopSegment()
+            console.log('[speak: onerror]', error)
+            da.stopSegment()
         }
     })
 }
